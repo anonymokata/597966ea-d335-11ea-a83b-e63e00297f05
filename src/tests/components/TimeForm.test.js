920e0420fd8d991d416f529bb05de26f9278c16d
someone <someone>
2020-06-09 12:30:10 -0500

    duration calculates correclty off default times

--- src/tests/components/TimeForm.test.js
+++ src/tests/components/TimeForm.test.js
@@ -14,3 +14,8 @@ test('Should initialize form with start time 5:00pm and end time 04:00am', () =>
   expect(wrapper.state('startTime')).toBe('17:00');
   expect(wrapper.state('endTime')).toBe('04:00');
 });
+
+test('Should calculate initial duration at 660 minutes based off 5pm to 4am defaults', () => {
+  const wrapper = shallow( < TimeForm / > );
+  expect(wrapper.state('duration')).toBe(660);
+});
